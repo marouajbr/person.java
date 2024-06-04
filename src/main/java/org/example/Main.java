3c4170982c51e07aec391e033efd2e32c0ac03e6
DELL <marwa.jabroun.sordo@gmail.com>
2024-06-04 01:54:38 +0100

    person java

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    private static String Tetouan;
+    public static void main(String[] args) {
+        String firstName = "maroua";
+        String lastName = "ha";
+        int age = 18;
+        String address = "Tetouan" ;
+        String favoriteColor = "blue";
+        }
+    }
---
+++ src/main/java/person/person.java
@@ -0,0 +1,43 @@
+package person;
+
+public class person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String address;
+    private String favoriteColor;
+
+    public String getFirstName() {
+    return this.firstName;
+    }
+    public void setFirstName(String firstName) {
+    this.firstName = firstName;
+    }
+    public String getLastName() {
+        return this.lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public int getAge(int age) {
+        return this.age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getAddress() {
+        return this.address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getFavoriteColor() {
+        return this.favoriteColor;
+    }
+    public void setFavoriteColor(String favoriteColor) {
+        this.favoriteColor = favoriteColor;
+    }
+}
+
+
